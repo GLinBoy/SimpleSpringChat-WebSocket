6050ae1aefd902a9f3849002931ebbb5c7c3a49a
GLinBoy <hojjatabedie@gmail.com>
2021-04-03 20:17:01 +0430

    🕯 START: Project initiation and setup structure

---
+++ src/main/java/com/glinboy/test/chat/websocket/SimpleSpringChatWebSocketApplication.java
@@ -0,0 +1,13 @@
+package com.glinboy.test.chat.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringChatWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringChatWebSocketApplication.class, args);
+	}
+
+}
